470ee9db212b195d1f821c07e6b7a25c043fb924
jordan <jordanfear@chompsmedia.com>
2019-06-16 01:58:25 +0800

    add partner

--- public/js/form.js
+++ public/js/form.js
@@ -1,75 +1,115 @@
-$(document).ready(function() {
-    $( "select#employment-type" ).change(function() {
-        if(
-            $( "select#employment-type" ).val() == 1 ||
-            $( "select#employment-type" ).val() == 2 ||
-            $( "select#employment-type" ).val() == 3 ||
-            $( "select#employment-type" ).val() == 4 ||
-            $( "select#employment-type" ).val() == 10 
-        ){
-            $( "#employment-since" ).prop( "disabled", false );
-            $( "#employer-name" ).prop( "disabled", false );
+$(document).ready(function () {
+    $("select#employment-type").change(function () {
+        if (
+            $("select#employment-type").val() == 1 ||
+            $("select#employment-type").val() == 2 ||
+            $("select#employment-type").val() == 3 ||
+            $("select#employment-type").val() == 4 ||
+            $("select#employment-type").val() == 10
+        ) {
+            $("#employment-since").prop("disabled", false);
+            $("#employer-name").prop("disabled", false);
         } else {
-            $( "#employment-since" ).prop( "disabled", true );
-            $( "#employer-name" ).prop( "disabled", true );
+            $("#employment-since").prop("disabled", true);
+            $("#employer-name").prop("disabled", true);
         }
     });
 
-    $( "#citizen" ).change(function() {
-        if(
+    $("#citizen").change(function () {
+        if (
             $('#citizen').is(":checked")
-        ){
-            $( "#country" ).prop( "disabled", true );
-            $( "#years-in-norway" ).prop( "disabled", true );
+        ) {
+            $("#country").prop("disabled", true);
+            $("#years-in-norway").prop("disabled", true);
         } else {
-            $( "#country" ).prop( "disabled", false );
-            $( "#years-in-norway" ).prop( "disabled", false);
+            $("#country").prop("disabled", false);
+            $("#years-in-norway").prop("disabled", false);
         }
-        
+
     });
 
-    $( "#civil-status" ).change(function() {
+
+    $("select#employment-type-c").change(function () {
         if (
-            $( "select#civil-status" ).val() == 1 || 
-            $( "select#civil-status" ).val() == 5 
-        ){
-            $( "#spouse-income" ).prop( "disabled", false );
+            $("select#employment-type-c").val() == 1 ||
+            $("select#employment-type-c").val() == 2 ||
+            $("select#employment-type-c").val() == 3 ||
+            $("select#employment-type-c").val() == 4 ||
+            $("select#employment-type-c").val() == 10
+        ) {
+            $("#employment-since-c").prop("disabled", false);
+            $("#employer-name-c").prop("disabled", false);
         } else {
-            $( "#spouse-income" ).prop( "disabled", true );
+            $("#employment-since-c").prop("disabled", true);
+            $("#employer-name-c").prop("disabled", true);
         }
     });
 
-    $( "#living-status" ).change(function() {
+    $("#citizen-c").change(function () {
         if (
-            $( "select#living-status" ).val() == 1 || 
-            $( "select#living-status" ).val() == 3
-        ){
-            $( "#rent" ).prop( "disabled", false );
-            $( "#rent-received" ).prop( "disabled", false );
-            $( "#mortgage" ).prop( "disabled", false );
-        } else if (
-            $( "select#living-status" ).val() == 2
-        ){
-            $( "#rent" ).prop( "disabled", true );
-            $( "#rent-received" ).prop( "disabled", false );
-            $( "#mortgage" ).prop( "disabled", false );
-        } else if (
-            $( "select#living-status" ).val() == 4 ||
-            $( "select#living-status" ).val() == 5
-            
-        ){
-            $( "#rent" ).prop( "disabled", false );
-            $( "#rent-received" ).prop( "disabled", true );
-            $( "#mortgage" ).prop( "disabled", true );
+            $('#citizen-c').is(":checked")
+        ) {
+            $("#country-c").prop("disabled", true);
+            $("#years-in-norway-c").prop("disabled", true);
         } else {
-            $( "#rent" ).prop( "disabled", false );
-            $( "#rent-received" ).prop( "disabled", false );
-            $( "#mortgage" ).prop( "disabled", false );
+            $("#country-c").prop("disabled", false);
+            $("#years-in-norway-c").prop("disabled", false);
+        }
+
+    });
+
+    $("#civil-status").change(function () {
+        if (
+            $("select#civil-status").val() == 1 ||
+            $("select#civil-status").val() == 5
+        ) {
+            $("#spouse-income").prop("disabled", false);
+        } else {
+            $("#spouse-income").prop("disabled", true);
         }
     });
 
+    $("#living-status").change(function () {
+        if (
+            $("select#living-status").val() == 1 ||
+            $("select#living-status").val() == 3
+        ) {
+            $("#rent").prop("disabled", false);
+            $("#rent-received").prop("disabled", false);
+            $("#mortgage").prop("disabled", false);
+        } else if (
+            $("select#living-status").val() == 2
+        ) {
+            $("#rent").prop("disabled", true);
+            $("#rent-received").prop("disabled", false);
+            $("#mortgage").prop("disabled", false);
+        } else if (
+            $("select#living-status").val() == 4 ||
+            $("select#living-status").val() == 5
 
-    
+        ) {
+            $("#rent").prop("disabled", false);
+            $("#rent-received").prop("disabled", true);
+            $("#mortgage").prop("disabled", true);
+        } else {
+            $("#rent").prop("disabled", false);
+            $("#rent-received").prop("disabled", false);
+            $("#mortgage").prop("disabled", false);
+        }
+
+    });
+
+
+    $("#co-applicant").change(function () {
+        if (
+            $('#co-applicant').is(":checked")
+        ) {
+            $('#co-hab').removeClass("hide")
+        } else {
+            $('#co-hab').addClass("hide")  
+        }
+
+    });
 
 
 
@@ -78,7 +118,10 @@ $(document).ready(function() {
 
 
 
-    
+
+
+
+
 
 
 
